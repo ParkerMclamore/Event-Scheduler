03fed3b4e03bc7aa625c0c0305e828dec765c72e
Parkmclamore <parkmclamore@gmail.com>
2017-12-18 10:04:25 -0700

    parkersupdate

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -11,3 +11,16 @@
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+$(document).ready(function() {
+        $(document).delegate('.open', 'click', function(event){
+            $(this).addClass('oppenned');
+            event.stopPropagation();
+        })
+        $(document).delegate('body', 'click', function(event) {
+            $('.open').removeClass('oppenned');
+        })
+        $(document).delegate('.cls', 'click', function(event){
+            $('.open').removeClass('oppenned');
+            event.stopPropagation();
+        });
+    });
